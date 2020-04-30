cdb9c1514dd9a3a9f042a12dfb81fbdd97750b7b
bilal <bilal.khan535765@gmail.com>
2020-04-30 20:21:04 +0500

    first version

---
+++ src/app/view-student-profile/view.student.profile.component.ts
@@ -0,0 +1,6 @@
+import {Component} from '@angular/core';
+@Component({
+  selector: 'app-view-student-profile',
+  templateUrl: './view.student.profile.component.html'
+})
+export class ViewStudentProfileComponent {}
